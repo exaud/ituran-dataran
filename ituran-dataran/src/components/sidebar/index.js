4b6070e379cfefa37d9921c8a1b8af7f67938d0f
Marcio Rocha <marcio.rocha@exaud.com>
2023-09-06 16:50:48 +0100

    Improve sidebar css styling

--- ituran-dataran/src/components/sidebar/index.js
+++ ituran-dataran/src/components/sidebar/index.js
@@ -45,62 +45,60 @@ function Sidebar({ }) {
     }
 
     return (
-        <div className='sidebar-wrapper'>
-            <div className='sidebar'>
-                <div className='sidebar-column' onMouseOver={() => toggleSidebar(true)} onMouseOut={() => toggleSidebar(false)}>
-                    <div className='sidebar-container'>
-                        <button className='sidebar-button' onClick={() => setButtonSelected(0)}>
-                            <div className={`sidebar-button-icon ${buttonSelected === 0 ? " current" : ""}`}>
-                                <HomeIcon className='svg-container' />
-                            </div>
-                            <div className={`sidebar-button-title ${buttonSelected === 0 ? " current" : ""}`}>
-                                <Link onClick={() => window.location.href = "/"}>Home</Link>
-                            </div>
-                        </button>
-                        <button className='sidebar-button' onClick={() => setButtonSelected(1)}>
-                            <div className={`sidebar-button-icon ${buttonSelected === 1 ? " current" : ""}`}>
-                                <DashboardsIcon className='svg-container' />
-                            </div>
-                            <div className={`sidebar-button-title ${buttonSelected === 1 ? " current" : ""}`}>
-                                <Link onClick={() => window.location.href = "/dashboards/main-dashboard"}>Dashboards</Link>
-                            </div>
-                        </button>
-                        {buttonSelected === 1 && hoveringSidebar ?
-                            <div className='sibebar-section-sub-options'>
-                                <button className={`sidebar-sub-button ${subButtonSelected === 0 ? " current-sub-option" : ""}`}>
-                                    <Link onClick={() => window.location.href = "/dashboards/main-dashboard"}>Main Dashboard</Link>
-                                </button>
-                                <button className={`sidebar-sub-button ${subButtonSelected === 1 ? " current-sub-option" : ""}`}>
-                                    <Link onClick={() => window.location.href = "/dashboards/vehicle-geo-insights"}>Vehicle Geo Insights</Link>
-                                </button>
-                                <button className={`sidebar-sub-button ${subButtonSelected === 2 ? " current-sub-option" : ""}`}>
-                                    <Link onClick={() => window.location.href = "/dashboards/daily-insights"}>Daily Insights</Link>
-                                </button>
-                                <button className={`sidebar-sub-button ${subButtonSelected === 3 ? " current-sub-option" : ""}`}>
-                                    <Link onClick={() => window.location.href = "/dashboards/vehicle-insights"}>Vehicle Insights</Link>
-                                </button>
-                                <button className={`sidebar-sub-button ${subButtonSelected === 4 ? " current-sub-option" : ""}`}>
-                                    <Link onClick={() => window.location.href = "/dashboards/safety-events"}>Safety Events</Link>
-                                </button>
-                            </div>
-                            : null}
-                        <button className='sidebar-button' onClick={() => setButtonSelected(2)}>
-                            <div className={`sidebar-button-icon ${buttonSelected === 2 ? " current" : ""}`}>
-                                <ReportsIcon className='svg-container' />
-                            </div>
-                            <div className={`sidebar-button-title ${buttonSelected === 2 ? " current" : ""}`}>
-                                <Link onClick={() => window.location.href = "/reports"}>Reports</Link>
-                            </div>
-                        </button>
-                        <button className='sidebar-button' onClick={() => setButtonSelected(3)}>
-                            <div className={`sidebar-button-icon ${buttonSelected === 3 ? " current" : ""}`}>
-                                <SettingsIcon className='svg-container' />
-                            </div>
-                            <div className={`sidebar-button-title ${buttonSelected === 3 ? " current" : ""}`}>
-                                <Link onClick={() => window.location.href = "/settings"}>Settings</Link>
-                            </div>
-                        </button>
-                    </div>
+        <div className='sidebar'>
+            <div className='sidebar-column' onMouseOver={() => toggleSidebar(true)} onMouseOut={() => toggleSidebar(false)}>
+                <div className='sidebar-container'>
+                    <button className='sidebar-button' onClick={() => setButtonSelected(0)}>
+                        <div className={`sidebar-button-icon ${buttonSelected === 0 ? " current" : ""}`}>
+                            <HomeIcon className='svg-container' />
+                        </div>
+                        <div className={`sidebar-button-title ${buttonSelected === 0 ? " current" : ""}`}>
+                            <Link onClick={() => window.location.href = "/"}>Home</Link>
+                        </div>
+                    </button>
+                    <button className='sidebar-button' onClick={() => setButtonSelected(1)}>
+                        <div className={`sidebar-button-icon ${buttonSelected === 1 ? " current" : ""}`}>
+                            <DashboardsIcon className='svg-container' />
+                        </div>
+                        <div className={`sidebar-button-title ${buttonSelected === 1 ? " current" : ""}`}>
+                            <Link onClick={() => window.location.href = "/dashboards/main-dashboard"}>Dashboards</Link>
+                        </div>
+                    </button>
+                    {buttonSelected === 1 && hoveringSidebar ?
+                        <div className='sibebar-section-sub-options'>
+                            <button className={`sidebar-sub-button ${subButtonSelected === 0 ? " current-sub-option" : ""}`}>
+                                <Link onClick={() => window.location.href = "/dashboards/main-dashboard"}>Main Dashboard</Link>
+                            </button>
+                            <button className={`sidebar-sub-button ${subButtonSelected === 1 ? " current-sub-option" : ""}`}>
+                                <Link onClick={() => window.location.href = "/dashboards/vehicle-geo-insights"}>Vehicle Geo Insights</Link>
+                            </button>
+                            <button className={`sidebar-sub-button ${subButtonSelected === 2 ? " current-sub-option" : ""}`}>
+                                <Link onClick={() => window.location.href = "/dashboards/daily-insights"}>Daily Insights</Link>
+                            </button>
+                            <button className={`sidebar-sub-button ${subButtonSelected === 3 ? " current-sub-option" : ""}`}>
+                                <Link onClick={() => window.location.href = "/dashboards/vehicle-insights"}>Vehicle Insights</Link>
+                            </button>
+                            <button className={`sidebar-sub-button ${subButtonSelected === 4 ? " current-sub-option" : ""}`}>
+                                <Link onClick={() => window.location.href = "/dashboards/safety-events"}>Safety Events</Link>
+                            </button>
+                        </div>
+                        : null}
+                    <button className='sidebar-button' onClick={() => setButtonSelected(2)}>
+                        <div className={`sidebar-button-icon ${buttonSelected === 2 ? " current" : ""}`}>
+                            <ReportsIcon className='svg-container' />
+                        </div>
+                        <div className={`sidebar-button-title ${buttonSelected === 2 ? " current" : ""}`}>
+                            <Link onClick={() => window.location.href = "/reports"}>Reports</Link>
+                        </div>
+                    </button>
+                    <button className='sidebar-button' onClick={() => setButtonSelected(3)}>
+                        <div className={`sidebar-button-icon ${buttonSelected === 3 ? " current" : ""}`}>
+                            <SettingsIcon className='svg-container' />
+                        </div>
+                        <div className={`sidebar-button-title ${buttonSelected === 3 ? " current" : ""}`}>
+                            <Link onClick={() => window.location.href = "/settings"}>Settings</Link>
+                        </div>
+                    </button>
                 </div>
             </div>
         </div>
