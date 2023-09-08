fb416822fc83db08bc0cdc241b448fc61ccd4662
Marcio Rocha <marcio.rocha@exaud.com>
2023-09-08 17:44:45 +0100

    Update dashboards' iframes

--- ituran-dataran/src/components/pages/dashboards/index.js
+++ ituran-dataran/src/components/pages/dashboards/index.js
@@ -8,15 +8,15 @@ function Dashboards({ }) {
     function renderSwitch(dashboard) {
         switch (dashboard) {
             case 'main-dashboard':
-                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYTQ2ZTkyMTctZWI5MC00MGUxLWIzYzEtYjA5MDBiNjM3ZTM1IiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSectionc857173daa1ec4d9cfb4"></iframe>
+                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiOWYxYTEzNjAtNGZiMy00M2NhLWIyMWEtMTk1YWI5NjU1NDRmIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSectionc857173daa1ec4d9cfb4"></iframe>
             case 'vehicle-geo-insights':
-                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYTQ2ZTkyMTctZWI5MC00MGUxLWIzYzEtYjA5MDBiNjM3ZTM1IiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection6ee02bd568d797d00dc9"></iframe>
+                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiOWYxYTEzNjAtNGZiMy00M2NhLWIyMWEtMTk1YWI5NjU1NDRmIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection6ee02bd568d797d00dc9"></iframe>
             case 'daily-insights':
                 return <p>Nothing to Display</p>
             case 'vehicle-insights':
                 return <p>Nothing to Display</p>
             case 'safety-events':
-                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYTQ2ZTkyMTctZWI5MC00MGUxLWIzYzEtYjA5MDBiNjM3ZTM1IiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection03cf3b0476036f7c1eb5"></iframe>
+                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiOWYxYTEzNjAtNGZiMy00M2NhLWIyMWEtMTk1YWI5NjU1NDRmIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection03cf3b0476036f7c1eb5"></iframe>
         }
     }
 
