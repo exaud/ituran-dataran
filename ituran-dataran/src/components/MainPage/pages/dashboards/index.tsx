14663e1ba56324cbfc83a3b4241be0a8d58aafb9
Fabio Morais <fabio.morais@exaud.com>
2023-10-17 10:58:36 +0100

    added the new buttons for macro pages. remove the insight buttons.

--- ituran-dataran/src/components/MainPage/pages/dashboards/index.tsx
+++ ituran-dataran/src/components/MainPage/pages/dashboards/index.tsx
@@ -10,14 +10,20 @@ function Dashboards() : JSX.Element {
         switch (dashboard) {
             case 'main-dashboard':
                 return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYWU4YTYyODEtMWVjZC00NDVjLTllM2MtOTQ4ZGZlZTZlM2RhIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSectionc857173daa1ec4d9cfb4"></iframe>
-            case 'vehicle-geo-insights':
-                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYWU4YTYyODEtMWVjZC00NDVjLTllM2MtOTQ4ZGZlZTZlM2RhIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection6ee02bd568d797d00dc9"></iframe>
-            case 'daily-insights':
+            case 'macro-dashboard':
                 return <p>Nothing to Display</p>
-            case 'vehicle-insights':
+            case 'vehicle-geo':
+                return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYWU4YTYyODEtMWVjZC00NDVjLTllM2MtOTQ4ZGZlZTZlM2RhIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection6ee02bd568d797d00dc9"></iframe>
+            case 'macro-geo':
                 return <p>Nothing to Display</p>
             case 'safety-events':
                 return <iframe className="iframe-content" title="Ituran Dataran Reports Clean" height="2000" src="https://app.powerbi.com/view?r=eyJrIjoiYWU4YTYyODEtMWVjZC00NDVjLTllM2MtOTQ4ZGZlZTZlM2RhIiwidCI6IjgxYjZjZTg5LWM3YjktNDM2ZC1hN2MzLWQ5N2ZmN2NkM2ZjMiIsImMiOjl9&pageName=ReportSection03cf3b0476036f7c1eb5"></iframe>
+            case 'macro-safety':
+                return <p>Nothing to Display</p>
+/*             case 'daily-insights':
+                return <p>Nothing to Display</p>
+            case 'vehicle-insights':
+                return <p>Nothing to Display</p> */
         }
     }
 
