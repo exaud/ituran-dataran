911435d0217a79225b3c2aef49111e6123e6ce67
Fabio Morais <fabio.morais@exaud.com>
2024-01-09 18:44:41 +0000

    Re-added Changes to reports page for testing.

--- ituran-dataran/src/components/MainPage/Pages/Reports/Reports.tsx
+++ ituran-dataran/src/components/MainPage/Pages/Reports/Reports.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { Button } from '@mui/base';
 import './Reports.css'
+import MetricsList from "./MetricsList/MetricsList";
+import TimeChart from "./TimeChart/TImeChart";
+import RegionChart from "./RegionChart/RegionChart";
+import TypeChart from "./TypeChart/TypeChart";
 
 function Reports() : JSX.Element {
+
+    function handleClick(){
+        console.log("Create Report");
+    }
+
+
     return (
             <div className='page-content'>
-                <p>Reports</p>
+                <div className='reports-page-heading-container'>
+                    <p className='reports-page-heading'>My Reports</p>
+                    <Button className="new-report-button" onClick={handleClick}> New Report</Button>
+                </div>
+                <div className='reports-page-body-container'>
+                    <p>ADD TAB CONTAINER</p>
+                    <p>ADD TABLE CONTAINER</p>
+                    <div className="reports-page-metrics-container">
+                        <TimeChart />
+                        <RegionChart />
+                        <TypeChart />
+                        <MetricsList/>
+                    </div>
+                </div>
+
             </div>
-    );
+    );   
 }
 export default Reports
